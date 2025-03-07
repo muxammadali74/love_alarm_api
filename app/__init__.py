1eda79867c0dae4ed02508f21e96e377f7497a74
muxammadali74 <160300791+muxammadali74@users.noreply.github.com>
2025-03-08 00:57:41 +0500

    Deploy to Render

---
+++ app/__init__.py
@@ -0,0 +1,2 @@
+from .database import get_db_connection, init_db
+from .api import router
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.api.v1 import router
-from app.database import init_db
+from app import router
 
+from app import init_db
 
 app = FastAPI(title="Love Alarm API")
 app.include_router(router, prefix='/v1')
 init_db()
+
 
